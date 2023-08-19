9523e44d4673aa95c8b023f6b685d159a4072bd8
ssi02014 <ssi02014@naver.com>
2023-08-20 00:38:43 +0900

    docs: Add Introduce Page

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -97,7 +97,7 @@ const config = {
             items: [
               {
                 label: 'Getting Started',
-                to: '/docs/category/devgraceui',
+                to: '/docs/category/devgracereact',
               },
             ],
           },
--- docs/i18n/ko/docusaurus-plugin-content-pages/index.tsx
+++ docs/i18n/ko/docusaurus-plugin-content-pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/devgraceui">
+            to="/docs/category/devgracereact">
             Getting Started
           </Link>
         </div>
