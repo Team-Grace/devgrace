eb3e7929ba7c171f95771d4e3789716cbbc9aafc
ssi02014 <ssi02014@naver.com>
2023-08-22 02:23:24 +0900

    test: github action test commit

--- packages/ui/src/components/Button/index.tsx
+++ packages/ui/src/components/Button/index.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 interface ButtonProps {
   children: React.ReactNode;
 }
+
 export const Button = ({ children }: ButtonProps) => {
   return <StyledButton>{children}</StyledButton>;
 };
