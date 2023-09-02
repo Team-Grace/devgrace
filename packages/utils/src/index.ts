24ab04932ebc175d96bb8a608df5ff983525113b
ssi02014 <ssi02014@naver.com>
2023-09-03 02:12:28 +0900

    feat(utils): Add removeSpecialCharacters, countSubstringOccurrences

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,3 @@
 export * from './common';
 export * from './device';
-export * from './format';
+export * from './string';
--- packages/utils/src/string/index.ts
+++ packages/utils/src/string/index.ts
@@ -1,3 +1,5 @@
+export * from './countSubstringOccurrences';
 export * from './extractNumber';
 export * from './formatPhoneNumber';
 export * from './formatNumberWithCommas';
+export * from './removeSpecialCharacters';
