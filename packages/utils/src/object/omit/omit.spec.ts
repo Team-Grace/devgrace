ef68375971968ab1c152ab8b303e8e0f04c353b2
ssi02014 <ssi02014@naver.com>
2023-10-25 23:24:21 +0900

    test(utils): fix omit test code

--- packages/utils/src/object/omit/omit.spec.ts
+++ packages/utils/src/object/omit/omit.spec.ts
@@ -11,7 +11,7 @@ describe('omit function', () => {
 
   it('should return a new object omitting multiple keys from an object', () => {
     const symbol = Symbol('d');
-    const inputObj = { a: 1, b: 2, c: 3, d: 4, [symbol]: 4 } as const;
+    const inputObj = { a: 1, b: 2, c: 3, d: 4, [symbol]: 5 } as const;
     const omittedObj = omit(inputObj, ['b', 'd']);
 
     expect(omittedObj).toEqual({ a: 1, c: 3 });
@@ -19,7 +19,7 @@ describe('omit function', () => {
 
   it('should return a new object that is deeply copied', () => {
     const symbol = Symbol('d');
-    const inputObj = { a: 1, b: { x: 2, y: 3 }, c: 4, [symbol]: 4 } as const;
+    const inputObj = { a: 1, b: { x: 2, y: 3 }, c: 4, [symbol]: 5 } as const;
     const omittedObj = omit(inputObj, ['a', 'c']);
 
     expect(omittedObj.b).not.toBe(inputObj.b);
