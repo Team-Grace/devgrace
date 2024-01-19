1fd4c3c863381af5b94fa261c4a4847063cd0a61
ssi02014 <ssi02014@naver.com>
2024-01-19 17:39:45 +0900

    docs: Add useMediaQuery Hook docs

--- packages/react/src/hooks/useMediaQuery/index.tsx
+++ packages/react/src/hooks/useMediaQuery/index.tsx
@@ -1,18 +1,19 @@
 import { isClient } from '@devgrace/utils';
 import { useCallback, useEffect, useState } from 'react';
 
-const useMediaQuery = (query: string) => {
-  const getMatchMedia = (query: string) => {
-    if (isClient()) {
-      return window.matchMedia(query).matches;
-    }
-    return false;
-  };
+const getMatchMedia = (query: string) => {
+  if (isClient()) {
+    return window.matchMedia(query).matches;
+  }
+  return false;
+};
+
+export const useMediaQuery = (query: string) => {
   const [isMatch, setIsMatch] = useState(getMatchMedia(query));
 
   const handleChange = useCallback(() => {
     setIsMatch(getMatchMedia(query));
-  }, [query, getMatchMedia]);
+  }, [query]);
 
   useEffect(() => {
     const matchMedia = window.matchMedia(query);
@@ -25,4 +26,16 @@ const useMediaQuery = (query: string) => {
   return { isMatch };
 };
 
-export default useMediaQuery;
+export const Example = () => {
+  const { isMatch } = useMediaQuery('(min-width: 768px)');
+  return (
+    <div>
+      <p>브라우저 너비를 조정해보세요!</p>
+      <p>
+        {isMatch
+          ? 'viewport 너비가 768px 이상입니다.'
+          : 'viewport 너비가 768px 미만입니다.'}
+      </p>
+    </div>
+  );
+};
