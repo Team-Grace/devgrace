ae35b28a3c09edc4a517569bfafc8731a8fc5e52
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-04-23 23:49:15 +0900

    feat(react): usePreservedState (#66)

---
+++ packages/react/src/hooks/usePreservedState/usePreservedState.spec.ts
@@ -0,0 +1,81 @@
+import { renderHook } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { usePreservedState } from '.';
+
+describe('usePreservedState', () => {
+  describe('Default Comparator Callback', () => {
+    it('should maintain the same state and reference if the values are identical.', () => {
+      const originObj = { group: 'modern-agile' };
+      const targetObj = { group: 'modern-agile' };
+
+      const { result, rerender } = renderHook(
+        ({ value }) => usePreservedState(value),
+        {
+          initialProps: { value: originObj },
+        }
+      );
+
+      rerender({ value: targetObj });
+
+      expect(result.current).toBe(originObj);
+      expect(result.current).toEqual(originObj);
+    });
+
+    it('should update the state if the values change.', () => {
+      const originObj = { group: 'modern-agile' };
+      const targetObj = { group: 'frontend' };
+
+      const { result, rerender } = renderHook(
+        ({ value }) => usePreservedState(value),
+        {
+          initialProps: { value: originObj },
+        }
+      );
+
+      rerender({ value: targetObj });
+
+      expect(result.current).toBe(targetObj);
+      expect(result.current).toEqual(targetObj);
+    });
+  });
+
+  describe('Custom Comparator Callback', () => {
+    it('should not update the state and should maintain the reference if the comparator returns true.', () => {
+      const comparator = (a: any, b: any) => a.group === b.group;
+
+      const originObj = { group: 'modern-agile', title: 'origin' };
+      const changedObj = { group: 'modern-agile', title: 'change' };
+
+      const { result, rerender } = renderHook(
+        ({ value }) => usePreservedState(value, comparator),
+        {
+          initialProps: { value: originObj },
+        }
+      );
+
+      rerender({ value: changedObj });
+
+      expect(result.current).toBe(originObj);
+      expect(result.current).toEqual(originObj);
+    });
+
+    it('should update the state if the comparator returns false.', () => {
+      const comparator = (a: any, b: any) => a.group === b.group;
+
+      const originObj = { group: 'modern-agile', title: 'origin' };
+      const changedObj = { group: 'frontend', title: 'change' };
+
+      const { result, rerender } = renderHook(
+        ({ value }) => usePreservedState(value, comparator),
+        {
+          initialProps: { value: originObj },
+        }
+      );
+
+      rerender({ value: changedObj });
+
+      expect(result.current).toBe(changedObj);
+      expect(result.current).toEqual(changedObj);
+    });
+  });
+});
